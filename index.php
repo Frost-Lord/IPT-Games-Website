e63134a46d65cbcdf8c0ee210678ef0bfce3e9d8
Glitched <51778028+DaKing01@users.noreply.github.com>
2022-08-09 20:56:57 +1000

    .

--- index.php
+++ index.php
@@ -32,47 +32,81 @@
     <a href="shoppingCart.php" class="shoppingcart"><i class="fa fa-shopping-cart"></i></a>
 </div>
 
+<br></br>
 
-<div>
-    <div id="img-right" class="img-right">  
-        <img src="https://wallpapershome.com/images/pages/pic_v/13886.jpg" alt="banner" class="img-fluid">
+<section class=" slider_section ">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail_box">
+                  <h1>
+                      ALL THE GAMES YOU WANT <br>
+                      IN ONE <span>PLACE!</span>
+                    </h1>
+                    <p>
+                     FrostProxy is a video game digital distribution service
+                    </p>
+                  </div>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                  <div class="img-box">
+                    <img src="https://i.ibb.co/37VJLgz/removal-ai-tmp-62f2332db0f0c.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </section>
+
+<br></br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-deck">
+                <?php
+                    $db_connection = pg_connect("host=localhost dbname=Games user=postgres password=password");
+                    if (!$db_connection) {
+                        die("Error in connection: " . pg_last_error());
+                    }
+                    $query = "SELECT * FROM listgames";
+                    $result = pg_query($query);
+                    if (!$result) {
+                        die("Error in SQL query: " . pg_last_error());
+                    }   
+                    $row = pg_fetch_array($result, null, PGSQL_ASSOC);
+                    $limit = 0;
+                    while ($row && $limit < 3) {
+                        echo '<div class="card">';
+                        echo '<img class="card-img-top" src="https://i.ibb.co/37VJLgz/removal-ai-tmp-62f2332db0f0c.png" alt="Card image cap">';
+                        echo '<div class="card-body">';
+                        echo '<h5 class="card-title">'.$row['title'].'</h5>';
+                        echo '<p class="card-text">'.$row['description'].'</p>';
+                        echo '<p class="card-text"><small class="text-muted">Cost: $'.$row['cost'].' AUD</small></p>';
+                        echo "<form action='index.php' method='post'>";
+                        echo "<input type='hidden' name='title' value='".$row['title']."'>";
+                        echo "<input type='hidden' name='description' value='".$row['description']."'>";
+                        echo "<input type='hidden' name='cost' value='".$row['cost']."'>";
+                        echo "<input type='submit' class='btn btn-primary' value='Add to Cart'>";
+                        echo "</form>";
+                        echo '</div>';
+                        echo '</div>';
+                        $row = pg_fetch_array($result, null, PGSQL_ASSOC);
+                    }              
+                ?>
+            </div>
+        </div>
     </div>
 </div>
-
-
-<style>
-.img-right {
-    position: absolute;
-    top: 53px;
-    right: 0px;
-    width: 15%;
-    height: 15%;
-    border-radius: 0px 0px 80px 80px;
-    animation-name: flow;
-    animation-duration: 4s;
-    animation-iteration-count: infinite;
-    animation-delay: -2s;
-    transition-timing-function: cubic-bezier(0,.57,1,.46);
-    box-shadow: 2px 2px 16px 2px rgba(0,0,0,0.5);
-  
-}
-.second-animation {
-  animation-delay: -1.5s;
-}
-@keyframes flow {
-  0% {height: 20%; border-radius: 0px 0px 30px 30px;}
-  50% {height: 90%; border-radius: 0px 0px 100px 100px;}
-  100% {height: 20%; border-radius: 0px 0px 30px 30px;}
-}
-</style>
-
-
-<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+<br></br>
 
 
 
 <a class="topPicks">
-<h1 style="color:white">Top Picks:</h1>
+<h1 style="color:white">ㅤㅤTop Picks:</h1>
 <br></br>
  <?php
  session_start();
@@ -97,13 +131,17 @@
             echo "<div class='card'>";
             echo "<h3>".$row['title']."</h3>";
             echo "<p>".$row['description']."</p>";
-            echo "<p>".$row['cost']."</p>";
+            if (!$row['price']) {
+                echo "<p></p>";
+            } else {
+                echo "<p>Cost: $".$row['cost']." AUD</p>";
+            }
          
             echo "<form action='index.php' method='post'>";
             echo "<input type='hidden' name='title' value='".$row['title']."'>";
             echo "<input type='hidden' name='description' value='".$row['description']."'>";
             echo "<input type='hidden' name='cost' value='".$row['cost']."'>";
-            echo "<input type='submit' value='Add to Cart'>";
+            echo "<input type='submit' class='btn btn-primary' value='Add to Cart'>";
             echo "</form>";
          
             echo "</div>";
@@ -120,37 +158,7 @@
     }
 
      echo "<br></br><br></br><br></br>";
-     //create a box for each game in the database and display the title, description, cost, and make it like a slideshow
-     $o = 0;
-     while ($o < 3) {
-         echo "<div class='row'>";
-         $j = 0;
-         while ($j < 3) {
-             echo "<div class='col'>";
-             echo "<div class='card'>";
-             echo "<h3>".$row['title']."</h3>";
-             echo "<p>".$row['description']."</p>";
-             echo "<p>".$row['cost']."</p>";
-          
-             echo "<form action='index.php' method='post'>";
-             echo "<input type='hidden' name='title' value='".$row['title']."'>";
-             echo "<input type='hidden' name='description' value='".$row['description']."'>";
-             echo "<input type='hidden' name='cost' value='".$row['cost']."'>";
-             echo "<input type='submit' value='Add to Cart'>";
-             echo "</form>";
-          
-             echo "</div>";
-             echo "</div>";
-             $row = pg_fetch_array($result, null, PGSQL_ASSOC);
-             $j++;
-             if ($j > 9) {
-                 break;
-             }
-         }
-         echo "</div>";
-         echo "<br></br>";
-         $o++;
-     }
+     
     
 
 
@@ -197,7 +205,7 @@
 ?>
 <style>
 .card {
-    background-color: #333;
+    background-color: #1b2838;
 }
 .row {
     display: flex;
@@ -209,6 +217,21 @@
 }
 .card-text {
     font-size: 1em;
+}
+.card:hover {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+}
+.btn {
+    background-color: #1b2838;
+    color: white;
+    padding: 12px;
+    margin: 10px 0;
+    border: none;
+    width: 100%;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 1.2em;
 }
 </style>
 </a>
